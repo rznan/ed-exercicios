547260bc9d4a0cf5e8f125e00c3b817bafbf03cf
Renan <109163037+rznan@users.noreply.github.com>
2024-03-24 18:00:50 -0300

    add solução da parte b do exercício 3 da lista 6 de pilhas

---
+++ Lista6/PilhaStrings/src/StackString.java
@@ -0,0 +1,47 @@
+import model.NodeString;
+
+public class StackString {
+    private NodeString top;
+
+    public StackString() {}
+
+    public boolean isEmpty() { return top == null; }
+
+    public void push(String value) {
+        if(isEmpty()) top = new NodeString(value);
+        else {
+            NodeString newTop = new NodeString(value);
+            newTop.setNext(top);
+            top = newTop;
+        }
+    }
+
+    public String pop() throws Exception {
+        if(isEmpty()) throw new Exception("Stack is empty");
+        else {
+            String value = top.getData();
+            top = top.getNext();
+            return value;
+        }
+    }
+
+    public String top() throws Exception {
+        if(isEmpty()) throw new Exception("Stack is empty");
+        else {
+            return top.getData();
+        }
+    }
+
+    public int size() {
+        int cont = 0;
+        if(!isEmpty()) {
+            NodeString aux = top.getNext();
+            while(aux != null) {
+                cont++;
+                aux = aux.getNext();
+            }
+        }
+        return cont;
+    }
+
+}
---
+++ Lista6/PilhaStrings/src/model/NodeString.java
@@ -0,0 +1,32 @@
+package model;
+
+public class NodeString {
+    private String data;
+    private NodeString next;
+
+    public NodeString(String data) { setData(data); }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public NodeString getNext() {
+        return next;
+    }
+
+    public void setNext(NodeString next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "NodeString{" +
+                "data='" + data + '\'' +
+                ", next=" + next +
+                '}';
+    }
+}
